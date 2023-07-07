654995179c8ac26c94e5ca13b6c42861472176ec
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2023-07-07 18:19:20 +0530

    testing the model

--- testing.py
+++ testing.py
@@ -7,9 +7,9 @@ import numpy as np
 
 vid = cv2.VideoCapture(0)
 # path = os.path.join("D:", "Projects", "models", "imageclassifier.h5")
-path = "D:\Projects\Machine Learning\ImageClassification\models\imageclassifier.h5"
+path = "D:\Projects\Machine Learning\ImageClassification\models\\fiveImageclassifier.h5"
 new_model = load_model(path)
-  
+emotion = ["angry", "fearful", "happy", "sad", "surpirse"]
 while(True):
       
     # Capture the video frame
@@ -18,11 +18,34 @@ while(True):
   
     resize = tf.image.resize(frame, (256,256))
     yhat = new_model.predict(np.expand_dims(resize/255, 0))
-    if(yhat > 0.5):
-        text = "Sad"
-    else:
-        text = "Happy"
-    cv2.putText(frame, text, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    prediction = yhat[0]
+    text = ""
+    max_prediction_text = ""
+    max_prediction = 0
+    if(prediction[0] > 0.7):
+        if (prediction[0] > max_prediction):
+            max_prediction_text = "Angry"
+        text += "Angry "
+    if(prediction[1] > 0.7):
+        if (prediction[1] > max_prediction):
+            max_prediction_text = "Fear"
+        text += "fear "
+    if(prediction[2] > 0.7):
+        if (prediction[2] > max_prediction):
+            max_prediction_text = "happy"
+        text += "happy "
+    if(prediction[3] > 0.7):
+        if (prediction[3] > max_prediction):
+            max_prediction_text = "Sad"
+        text += "sad "
+    if(prediction[4] > 0.7):
+        if (prediction[4] > max_prediction):
+            max_prediction_text = "Suprise"
+        text += "suprise "
+    for i in range(len(emotion)):
+        print(f"{emotion[i]}: {prediction[i]}")
+    cv2.putText(frame, max_prediction_text, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, text, (0, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.imshow('frame', frame)
       
     # the 'q' button is set as the
